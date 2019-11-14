2567dcf7170ca8cbfc4cfaa1150cf57f761f08b6
Anthony <anth19800@hotmail.com>
2019-11-14 15:05:32 -0500

    final

--- src/Cappuccino/ResourceManager.cpp
+++
@@ -1,8 +0,0 @@
-#include "Cappuccino/ResourceManager.h"
-
-namespace Cappuccino {
-
-	std::vector<Shader*>  ResourceManager::_allShaders =  {};
-	std::vector<Mesh*>    ResourceManager::_allMeshes =   {};
-	std::vector<Texture*> ResourceManager::_allTextures = {};
-}
